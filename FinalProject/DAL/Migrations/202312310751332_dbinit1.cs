c19752bafb6fafabf1f86b35d4d635f1c9f933ad
Shifar Tanjam <96732805+shifar282@users.noreply.github.com>
2023-12-31 14:35:07 +0600

    Image path remove

--- FinalProject/DAL/Migrations/202312310751332_dbinit1.cs
+++ FinalProject/DAL/Migrations/202312310751332_dbinit1.cs
@@ -1,18 +1,1 @@
-﻿namespace DAL.Migrations
-{
-    using System;
-    using System.Data.Entity.Migrations;
-    
-    public partial class dbinit1 : DbMigration
-    {
-        public override void Up()
-        {
-            DropColumn("dbo.Products", "ImagePath");
-        }
-        
-        public override void Down()
-        {
-            AddColumn("dbo.Products", "ImagePath", c => c.String());
-        }
-    }
-}
+﻿
