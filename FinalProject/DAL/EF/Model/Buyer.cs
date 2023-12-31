08853d4db9b7fcdcb85a4dc1fa89dee106800ed1
Md. Sadmanur Islam Shishir <109598070+shishir1290@users.noreply.github.com>
2023-12-31 20:50:24 +0600

    Validation

--- FinalProject/DAL/EF/Model/Buyer.cs
+++ FinalProject/DAL/EF/Model/Buyer.cs
@@ -11,17 +11,26 @@ namespace DAL.EF.Model
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+
         [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@gmail\.com$", ErrorMessage = "Email must be in the format sometings@gmail.com")]
         public string EmailAddress { get; set; }
-        [Required]
+
+        [Phone(ErrorMessage = "Invalid Phone Number")]
         public string PhoneNumber { get; set; }
-        [Required]
+        [Required, StringLength(1000)]
         public string Address { get; set; }
         [Required]
         public string Gender { get; set; }
+
         [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d.*\d)(?=.*\W.*\W)[A-Za-z\d\W]{8,}$",
+            ErrorMessage = "Password must have 1 uppercase, 1 lowercase, 2 symbols, 2 numbers, and be at least 8 characters long.")]
         public string Password { get; set; }
 
     }
---
+++ FinalProject/DAL/Migrations/202312311449331_init.cs
@@ -0,0 +1,30 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Buyers", "Name", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Buyers", "PhoneNumber", c => c.String());
+            AlterColumn("dbo.Buyers", "Address", c => c.String(nullable: false, maxLength: 1000));
+            AlterColumn("dbo.Products", "ProductName", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Description", c => c.String(nullable: false));
+            AlterColumn("dbo.Categories", "CategoryName", c => c.String(nullable: false));
+            AlterColumn("dbo.Categories", "CategoryDescription", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Categories", "CategoryDescription", c => c.String());
+            AlterColumn("dbo.Categories", "CategoryName", c => c.String());
+            AlterColumn("dbo.Products", "Description", c => c.String());
+            AlterColumn("dbo.Products", "ProductName", c => c.String());
+            AlterColumn("dbo.Buyers", "Address", c => c.String(nullable: false));
+            AlterColumn("dbo.Buyers", "PhoneNumber", c => c.String(nullable: false));
+            AlterColumn("dbo.Buyers", "Name", c => c.String(nullable: false));
+        }
+    }
+}
